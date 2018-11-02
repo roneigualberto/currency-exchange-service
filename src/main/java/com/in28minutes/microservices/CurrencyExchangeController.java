b5d156e1d343b192d169bd9c32d5da4340a59a90
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-11-02 08:28:37 -0400

    Update version

--- src/main/java/com/in28minutes/microservices/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/CurrencyExchangeController.java
@@ -2,11 +2,8 @@ package com.in28minutes.microservices;
 
 import static java.lang.Integer.parseInt;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
